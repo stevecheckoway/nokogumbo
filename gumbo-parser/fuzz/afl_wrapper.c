59eaa434d9292260eb9722ad95c3a48d75b428bc
Stephen Checkoway <s@pahtak.org>
2019-08-30 23:15:20 -0400

    Better fuzzer

--- gumbo-parser/fuzz/afl_wrapper.c
+++ gumbo-parser/fuzz/afl_wrapper.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     return 1;
   int fd = open(argv[1], O_RDONLY);
   if (fd < 0)
-    return 1;
+    return 2;
 
   size_t input_len = lseek(fd, 0, SEEK_END);
   (void)lseek(fd, 0, SEEK_SET);
@@ -21,11 +21,10 @@ int main(int argc, char *argv[])
   char *str = mmap(0, size, PROT_READ, MAP_FILE | MAP_PRIVATE, fd, 0);
   
   if (str == MAP_FAILED)
-    return 1;
+    return 3;
   GumboOutput *doc = gumbo_parse_with_options(&kGumboDefaultOptions, str,
                                               input_len);
   
-#if 0
   for (unsigned idx = 0; idx < doc->errors.length; ++idx)
   {
     GumboError *err = doc->errors.data[idx];
@@ -36,8 +35,7 @@ int main(int argc, char *argv[])
 
   gumbo_destroy_output(doc);
 
-  munmap(str, size);
-  close(fd);
-#endif
+  // munmap(str, size);
+  // close(fd);
   return 0;
 }
