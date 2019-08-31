09b7d7d5536fc0ae4ec9ecfff39cb15fa6722888
Stephen Checkoway <s@pahtak.org>
2019-08-31 15:37:59 -0400

    Allow resume

--- gumbo-parser/fuzz/afl_run.sh
+++ gumbo-parser/fuzz/afl_run.sh
@@ -9,6 +9,12 @@ dir=$(dirname "$0")
 output="${dir}/afl_output"
 afl_wrapper="${dir}/../build/afl_wrapper"
 
+inputdir="${dir}/afl"
+
+if [[ $# = 1 && "$1" = '-r' ]]; then
+  inputdir=-
+fi
+
 pid=()
 
 cleanup() {
@@ -20,8 +26,8 @@ cleanup() {
 trap "cleanup" EXIT
 
 for i in {1..3}; do
-  UBSAN_OPTIONS=halt_on_error=1:abort_on_error=1 afl-fuzz -x "${dictionary}" -i "${dir}/afl" -o "${output}" -S "fuzzer${i}" -- "${afl_wrapper}" @@ >/dev/null 2>/dev/null &
+  UBSAN_OPTIONS=halt_on_error=1:abort_on_error=1 afl-fuzz -x "${dictionary}" -i "${inputdir}" -o "${output}" -S "fuzzer${i}" -- "${afl_wrapper}" @@ >/dev/null 2>/dev/null &
   pid["$i"]=$!
 done
 
-UBSAN_OPTIONS=halt_on_error=1:abort_on_error=1 afl-fuzz -x "${dictionary}" -i "${dir}/afl" -o "${output}" -M fuzzer0 -- "${afl_wrapper}" @@
+UBSAN_OPTIONS=halt_on_error=1:abort_on_error=1 afl-fuzz -x "${dictionary}" -i "${inputdir}" -o "${output}" -M fuzzer0 -- "${afl_wrapper}" @@
