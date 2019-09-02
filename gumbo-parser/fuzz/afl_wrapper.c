c0fbf65b035694ddf3aac73043dfe7db8f0ba8bc
Stephen Checkoway <s@pahtak.org>
2019-09-02 10:44:26 -0400

    Use afl-clang-fast; add test cases

--- gumbo-parser/fuzz/afl_wrapper.c
+++ gumbo-parser/fuzz/afl_wrapper.c
@@ -9,33 +9,38 @@ int main(int argc, char *argv[])
 {
   if (argc != 2)
     return 1;
-  int fd = open(argv[1], O_RDONLY);
-  if (fd < 0)
-    return 2;
 
-  size_t input_len = lseek(fd, 0, SEEK_END);
-  (void)lseek(fd, 0, SEEK_SET);
-
-  size_t page_mask = sysconf(_SC_PAGESIZE) - 1;
-  size_t size = (input_len + page_mask) & ~page_mask;
-  char *str = mmap(0, size, PROT_READ, MAP_FILE | MAP_PRIVATE, fd, 0);
-  
-  if (str == MAP_FAILED)
-    return 3;
-  GumboOutput *doc = gumbo_parse_with_options(&kGumboDefaultOptions, str,
-                                              input_len);
-  
-  for (unsigned idx = 0; idx < doc->errors.length; ++idx)
+  while (__AFL_LOOP(1000))
   {
-    GumboError *err = doc->errors.data[idx];
-    char *error_text = 0;
-    (void)gumbo_caret_diagnostic_to_string(err, str, input_len, &error_text);
-    free(error_text);
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0)
+      return 2;
+
+    size_t input_len = lseek(fd, 0, SEEK_END);
+    (void)lseek(fd, 0, SEEK_SET);
+
+    size_t page_mask = sysconf(_SC_PAGESIZE) - 1;
+    size_t size = (input_len + page_mask) & ~page_mask;
+    char *str = mmap(0, size, PROT_READ, MAP_FILE | MAP_PRIVATE, fd, 0);
+
+    if (str == MAP_FAILED)
+      return 3;
+    GumboOutput *doc = gumbo_parse_with_options(&kGumboDefaultOptions, str,
+                                                input_len);
+
+    for (unsigned idx = 0; idx < doc->errors.length; ++idx)
+    {
+      GumboError *err = doc->errors.data[idx];
+      char *error_text = 0;
+      (void)gumbo_caret_diagnostic_to_string(err, str, input_len, &error_text);
+      free(error_text);
+    }
+
+    gumbo_destroy_output(doc);
+
+    munmap(str, size);
+    close(fd);
   }
 
-  gumbo_destroy_output(doc);
-
-  // munmap(str, size);
-  // close(fd);
   return 0;
 }
